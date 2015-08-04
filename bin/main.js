300c62cd92657c19e759ed054be0856da3b1fb64
Andre Barata <andrebarata@msn.com>
2015-08-04 15:43:01 +0100

    first files

---
+++ bin/main.js
@@ -0,0 +1,22 @@
+﻿//# sourceMappingURL=main.js.map
+
+var Teste = (function () {
+    function Teste() {
+        console.log("constructor called");
+    }
+    Teste.prototype.test = function () {
+        console.log("test called");
+    };
+    Teste.prototype.method2 = function () {
+        console.log("teste");
+    };
+    return Teste;
+})();
+
+console.log("hello");
+
+var a = new Teste();
+a.test();
+a.method2();
+
+
---
+++ main.ts
@@ -0,0 +1,18 @@
+﻿class Teste {
+    constructor() {
+        console.log("constructor called")
+    }
+    test() {
+        console.log("test called")
+    }
+    method2() {
+        console.log("teste");
+    }
+}
+
+console.log("hello");
+
+var a = new Teste();
+a.test();
+a.method2();
+
