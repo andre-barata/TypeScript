359539d08e773f0abc78a9f8fbc4478b37c251fb
Andre Barata <andrebarata@msn.com>
2015-08-14 16:11:25 +0100

    working bin utils

--- bin/main.js
+++ bin/main.js
@@ -1,24 +1,22 @@
 ﻿var Utils = (function () {
     function Utils() {
     }
-    Utils.toBin = function (value) {
+    Utils.numToBin = function (value) {
         var s = '';
         for (var i = 1 << 7; i != 0; i >>= 1)
             s += (value & i) > 0 ? "1" : "0";
         return s;
     };
-    Utils.fromBin = function (value) {
+    Utils.binToNum = function (value) {
         var ret = 0;
-        for (var i = value.length; i >= 0; i--)
-            if (value.charAt(i) === '1') {
-                ret += 2 ^ i;
-                console.log("added 2 ^ " + i + " = " + ret);
-            }
+        for (var i = 0; i < value.length; i++)
+            if (value.charAt(value.length - i - 1) === '1')
+                ret += 1 << i;
         return ret;
     };
     return Utils;
 })();
 
-console.log("123 to bin = " + Utils.toBin(123));
-console.log("110101  from bin = " + Utils.fromBin("110101"));
+console.log("123 to bin = " + Utils.numToBin(123));
+console.log("110101  from bin = " + Utils.binToNum("110101"));
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
 ﻿class Utils
 {
-	static toBin(value : number) : string
+	static numToBin(value : number) : string
 	{
 		var s : string = '';
 		for (var i : number = 1 << 7; i != 0; i >>= 1) s += (value & i) > 0 ? "1" : "0";
 		return s;
 	}
-	static fromBin(value: string) : number
+	static binToNum(value: string) : number
 	{
 		var ret : number = 0;
-		for (var i : number = value.length; i >= 0; i--) if (value.charAt(i) === '1') { ret += 2 ^ i; console.log("added 2 ^ " + i + " = " + ret); }
+		for (var i : number = 0; i < value.length; i++) if (value.charAt(value.length-i-1) === '1') ret += 1<<i;
 		return ret;
 	}
 }
 
-console.log("123 to bin = " + Utils.toBin(123));
-console.log("110101  from bin = " + Utils.fromBin("110101"));
+console.log("123 to bin = " + Utils.numToBin(123));
+console.log("110101  from bin = " + Utils.binToNum("110101"));
 
+
+	
+	
